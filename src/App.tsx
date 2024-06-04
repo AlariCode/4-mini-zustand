988d767d488593c385ae9bf84878138bd0fb1bd5
Serotonin-pepperonin <104297454+Serotonin-pepperonin@users.noreply.github.com>
2024-06-04 18:47:24 +0500

    3 - 2 Получение данных из State

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { useCounterStore } from "./model/counterStore";
 
 function App() {
-  return <></>;
+  const { counter } = useCounterStore();
+  return (
+    <div className="wrapper">
+      <span>{counter}</span>
+    </div>
+  );
 }
 
 export default App;
